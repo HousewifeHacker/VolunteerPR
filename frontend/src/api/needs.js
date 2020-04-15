6d3854c7a6f1080710f4da3e93ae00fd35e8997b
housewifehacker <housewifehacker@gmail.com>
2020-04-15 12:13:53 -0500

    bug fix api

--- frontend/src/api/needs.js
+++ frontend/src/api/needs.js
@@ -1,6 +1,6 @@
 import { getRequest } from ".";
 
-const baseUrl = "/api/v1/needs/";
+const baseUrl = "/api/v1/needs";
 
 export const listNeeds = (need_type) => {
   return getRequest(`${baseUrl}?type=${need_type}`);
